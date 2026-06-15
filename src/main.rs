d25f850467393b5d8a285f341207160c4e0bdcd2
Baodz <legiabao231237@gmail.com>
2026-06-14 20:21:05 -0500

    add crate, draw first thing, modify wgsl file

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,38 @@ use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::window::{Window, WindowId};
+use wgpu::util::{DeviceExt, BufferInitDescriptor};
+use std::time::{Duration, Instant};
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.5, -0.5, 0.0], color: [1.0, 0.0, 0.0], },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0], },
+    Vertex { position: [0.0, 0.5, 0.0], color: [0.0, 0.0, 1.0], },
+    Vertex { position: [0.5, 0.3, 0.0], color: [0.0, 1.0, 1.0], },
+    Vertex { position: [0.3, -0.5, 0.0], color: [1.0, 1.0, 0.0], },
+    Vertex { position: [-0.5, 0.5, 0.0], color: [0.0, 0.0, 0.0], },
+];
 struct WgpuCtx {
     surface: wgpu::Surface<'static>,
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
+    pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
 }
 
 #[derive(Default)]
 struct MyApp {
     window: Option<Arc<Window>>,
     gpu_ctx: Option<WgpuCtx>,
+    last_time: Option<Instant>,
 }
 
 impl WgpuCtx {
@@ -71,16 +92,63 @@ impl ApplicationHandler for MyApp {
                 view_formats: vec![],
             };
             surface.configure(&device, &config);
+
+            let vertex_buffer = device.create_buffer_init(&BufferInitDescriptor { 
+                label: Some("vx_buffer"), 
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            });
+
+            let shader = device.create_shader_module(wgpu::include_wgsl!("test.wgsl"));
+
+            let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("pipeline"),
+                layout: None,
+                vertex: wgpu::VertexState { 
+                    module: &shader, 
+                    entry_point: Some("vs_main"), 
+                    compilation_options: wgpu::PipelineCompilationOptions::default(), 
+                    buffers: &[wgpu::VertexBufferLayout {
+                        array_stride: 24 as u64,
+                        step_mode: wgpu::VertexStepMode::Vertex,
+                        attributes: &wgpu::vertex_attr_array![
+                            0 => Float32x3,
+                            1 => Float32x3,
+                        ],
+                    }], 
+                },
+                fragment: Some(wgpu::FragmentState { 
+                    module: &shader,
+                    entry_point: Some("fs_main"), 
+                    compilation_options: wgpu::PipelineCompilationOptions::default(),
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleStrip,
+                    ..Default::default()
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState::default(),
+                multiview_mask: None,
+                cache: None,
+            });
             WgpuCtx {
                 surface,
                 device,
                 queue,
                 config,
+                pipeline,
+                vertex_buffer,
             }
         });
 
         self.window = Some(window);
         self.gpu_ctx = Some(gpu_ctx);
+        self.last_time = Some(Instant::now());
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
@@ -89,30 +157,32 @@ impl ApplicationHandler for MyApp {
                 if let (Some(win), Some(gpu)) = (&self.window, &self.gpu_ctx) {
                     if let Some((frame, view)) = gpu.get_frame_view() {
 
-                    let mut encoder = gpu.device
-                    .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                        label: Some("Encoder"),
-                    });
+                        let mut encoder = gpu.device
+                        .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                            label: Some("Encoder"),
+                        });
 
-                    {
-                        let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                            label: Some("render pass"),
-                            color_attachments: &[Some(wgpu::RenderPassColorAttachment { 
-                                view: &view, 
-                                depth_slice: None, 
-                                resolve_target: None, 
+                        {
+                            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                                label: Some("render pass"),
+                                color_attachments: &[Some(wgpu::RenderPassColorAttachment { 
+                                    view: &view,
+                                    depth_slice: None, 
+                                    resolve_target: None, 
                                 ops: wgpu::Operations { 
                                     load: wgpu::LoadOp::Clear(wgpu::Color::WHITE), 
                                     store: wgpu::StoreOp::Store,
                                 },
-                            })],
-                            ..Default::default()
-                        });
+                                })],
+                                ..Default::default()
+                            });
+                            render_pass.set_pipeline(&gpu.pipeline);
+                            render_pass.set_vertex_buffer(0, gpu.vertex_buffer.slice(..));
+                            render_pass.draw(0..VERTICES.len() as u32, 0..1);
+                        }
+                        gpu.queue.submit(std::iter::once(encoder.finish()));
+                        frame.present();
                     }
-                    gpu.queue.submit(std::iter::once(encoder.finish()));
-                    frame.present();
-                }
-                    win.request_redraw();
                 } 
                 else {
                     println!("computer forced to run fast but the data?");
@@ -124,6 +194,18 @@ impl ApplicationHandler for MyApp {
             }
             _ => (),
         }
+    }
+    fn about_to_wait(&mut self, _event_loop: &ActiveEventLoop) {
+        if let (Some(win), Some(last_time)) = (&self.window, &self.last_time) {
+            let desire_time = Duration::from_secs_f64(1.0 / 60.0);
+            let time_eslaped = last_time.elapsed();
+            if time_eslaped < desire_time {
+                let sleep_time = desire_time - time_eslaped;
+                std::thread::sleep(sleep_time);
+            };
+            self.last_time = Some(Instant::now());
+            win.request_redraw();
+        };
     }
 }
 
--- src/test.wgsl
+++ src/test.wgsl
@@ -1,13 +1,22 @@
-struct FragOut { 
+struct VxIn {
+    @location(0) pos: vec3f,
+    @location(1) color: vec3f,
+}
+
+struct FragOut {
+    @builtin(position) pos: vec4f,
     @location(0) color: vec4f,
 }
 
 @vertex
-fn vs_main(color: FragOut) {
-
+fn vs_main(input: VxIn) -> FragOut {
+    return FragOut(
+        vec4f(input.pos, 1.0),
+        vec4f(input.color, 1.0)
+    );
 }
 
 @fragment
-fn fs_main(color: FragOut) -> @location(0) color: ver4f {
-    color.color
-}
+fn fs_main(in: FragOut) -> @location(0) vec4f {
+    return in.color;
+};
